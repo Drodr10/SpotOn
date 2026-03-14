00eeba7e9b9849fbac195e520aad129d6db62072
Adrian Estevez <adrianvez11@gmail.com>
2026-03-14 14:52:14 -0400

    Fixed Supabase export statement and environment variables names. (#20)
    * Rearranged fronted folder layout to match recommended expo layout. Added Supabse client object to use for frontend.
    Updated imports to reflect new folder layouts.
    
    * Fixed the supabase export statement and environment variable names. *WARNING* browser testing does not work properly, only test on the mobile app.

--- frontend/src/utils/supabase.ts
+++ frontend/src/utils/supabase.ts
@@ -4,13 +4,13 @@ import { createClient } from "@supabase/supabase-js";
 // The expo docs say that its okay to put the keys and url as an object literal since supabase has
 // RLS, but its probably better to just have it stored in an env regardless.
 
-const supabaseUrl = process.env.EXPO_SUPABASE_URL;
-const supabasePublishableKey = process.env.EXPO_SUPABASE_PUBLISHABLE_KEY;np
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "nulled";
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY ?? "nulled";
 
 //To use this:
 // import { supabase } from "../utils/supabase";
 
-createClient(supabaseUrl, supabasePublishableKey, {
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
     auth: {
         storage: localStorage,
         autoRefreshToken: true,
@@ -18,4 +18,3 @@ createClient(supabaseUrl, supabasePublishableKey, {
         detectSessionInUrl: false,
     }
 });
-
